86d538b9fc991f9a3bb81a40f1fd494ce747ed23
Neko <66227158+NicoNekoru@users.noreply.github.com>
2022-06-21 01:37:31 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "tID": "", // Change this to actual tournament ID for battlefy or challonge
+    "team1": "", "team2": "", // Change these to the two listed teams on the google forms
+    "sheetID": "", // Change this to the spreadsheet ID of the responses sheet https://docs.google.com/spreadsheets/d/****/
+	"username": "", // CHALLONGE: Change to username of Challonge API account
+	"apiKey": "" /// CHALLONGE: Change to Challonge API key
+}
